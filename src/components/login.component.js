78a16c37c982f551e8794ffaf6649ca9b2a27009
gasanchez10 <gsanchez@pastpost.com>
2022-10-10 23:06:48 -0500

    FInal

--- src/components/login.component.js
+++ src/components/login.component.js
@@ -1,23 +1,87 @@
 import React, { Component } from 'react'
-import { Navigate } from "react-router-dom";
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Alert from 'react-bootstrap/Alert';
+import Button from 'react-bootstrap/Button';
+import axios from "axios";
+const baseURL = "http://ec2-54-89-237-204.compute-1.amazonaws.com/api/v1/auth/authentication/login/access-token";
 
 
-export default class Login extends Component {
-  render() 
-  
- 
+class Login extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {mail: '', pass:'', show: false
+  };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChangePass = this.handleChangePass.bind(this);
+    this.setShow = this.setShow.bind(this);
+  }
+
+  setShow(){
+    this.setState({show: !this.state.show})
+  }
+
+  handleChange(event) {
+    this.setState({mail: event.target.value});
+  }
+
+  handleSubmit(event) {
+
+    var bodyFormData = new FormData();
+    bodyFormData.append('username', this.state.mail);
+    bodyFormData.append('password', this.state.pass); 
+
+    console.log(this.state)
+    event.preventDefault();
+    this.setShow()
+    axios({
+      method: "post",
+      url: baseURL,
+      data: bodyFormData,
+      headers: { "Content-Type": "multipart/form-data" },
+    }).then((response) => {
+      console.log("This is the response")
+      console.log(response)
+    });
+    // window.location.href='home' 
+
+  }
+
+
+  handleChangePass(event) {
+    this.setState({pass: event.target.value});
+  }
+
+
+
+ render()
+
+
   {
     return (
       <form>
-        <h3>Sign In</h3>
 
+<Alert show={this.state.show} variant="success">
+        <Alert.Heading>Error de verificacion!</Alert.Heading>
+        <p>
+          Ingresa la contraseña correcta!
+        </p>
+        <hr />
+        <div className="d-flex justify-content-end">
+        <Button onClick={this.setShow} variant="outline-success">
+           Cerrar
+          </Button>
+        </div>
+      </Alert>
+        <h3>Sign In</h3>
         <div className="mb-3">
-          <label>Email address</label>
+          <label>Email addresss</label>
           <input
             type="email"
             className="form-control"
             placeholder="Enter email"
+            value={this.state.mail}
+            onChange={this.handleChange}
           />
         </div>
 
@@ -27,31 +91,26 @@ export default class Login extends Component {
             type="password"
             className="form-control"
             placeholder="Enter password"
+            value={this.state.pass}
+            onChange={this.handleChangePass}
           />
         </div>
 
         <div className="mb-3">
           <div className="custom-control custom-checkbox">
-            <input
-              type="checkbox"
-              className="custom-control-input"
-              id="customCheck1"
-            />
-            <label className="custom-control-label" htmlFor="customCheck1">
-              Remember me
-            </label>
           </div>
         </div>
 
         <div className="d-grid">
-          <Link to={'/home'} type="submit" className="btn btn-primary">
+          <button onClick={this.handleSubmit} type="submit" className="btn btn-primary">
             Login
-          </Link>
+          </button>
         </div>
-        <p className="forgot-password text-right">
-          Forgot <a href="/">password?</a>
-        </p>
+    
       </form>
     )
   }
 }
+
+
+export default Login
